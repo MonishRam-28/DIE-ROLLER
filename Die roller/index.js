2f24d2261c8c30d5cbd39ed69df1df76f735020c
MonishRam-28 <102720637+MonishRam-28@users.noreply.github.com>
2024-05-25 11:37:17 +0530

    Add files via upload

---
+++ Die roller/index.js
@@ -0,0 +1,17 @@
+function rollDice(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const Images = [];
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        Images.push(`<img src="dice_images/${value}.png">`);
+    }
+
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = Images.join('');
+    
+}
